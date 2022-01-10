5fc62871758a4b6d7f1ff30cab7a95abbf72427f
riko1212 <46224717+riko1212@users.noreply.github.com>
2022-01-10 10:51:21 +0000

    Deploying to gh-pages from @ riko1212/bc-14-test@20316d66a1eafa65a6f1f66e8e095b9e16caa667 🚀

--- src.29b573d0.js
+++ src.29b573d0.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/bc-14-test/src.61f87c08.js.map
+//# sourceMappingURL=/bc-14-test/src.29b573d0.js.map
